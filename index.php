e9208e6d61581cad2e59de309c5ac04317900d5d
Viraj Chitnis <chitnisviraj@gmail.com>
2018-07-21 17:11:54 +0530

    Progress bars also indicate colors

--- index.php
+++ index.php
@@ -81,10 +81,17 @@
           }
           $memory_total_mb = number_format((float)$mem[1]/1024, 2, '.', '');
           $memory_used_mb = number_format((float)$mem[2]/1024, 2, '.', '');
+          $memory_progress_color = "bg-success";
+          if ($memory_usage_percent > 50) {
+            $memory_progress_color = "bg-warning";
+          }
+          if ($memory_usage_percent > 90) {
+            $memory_progress_color = "bg-danger";
+          }
           ?>
           <?php echo $memory_used_mb."/".$memory_total_mb." MB" ?>
           <div class="progress" style="height: 24px;">
-            <div class="progress-bar bg-success" role="progressbar" style="width: <?php echo $memory_usage_percent ?>%;" aria-valuenow="<?php echo $memory_usage_percent ?>" aria-valuemin="0" aria-valuemax="100"><?php echo $memory_usage_percent ?>%</div>
+            <div class="progress-bar <?php echo $memory_progress_color ?>" role="progressbar" style="width: <?php echo $memory_usage_percent ?>%;" aria-valuenow="<?php echo $memory_usage_percent ?>" aria-valuemin="0" aria-valuemax="100"><?php echo $memory_usage_percent ?>%</div>
           </div>
         </div>
       </div>
@@ -94,9 +101,19 @@
         </div>
         <div class="col border border-bottom-0" style="padding-top: 5px; padding-bottom: 5px;">
           <?php $disk_total = disk_total_space("/"); $disk_free = disk_free_space("/"); $disk_used = $disk_total - $disk_free; echo number_format((float)(((($disk_used)/1024)/1024)/1024), 2, '.', '')."/".number_format((float)((($disk_total/1024)/1024)/1024), 2, '.', '')." GB" ?>
-          <?php $disk_percentage = ($disk_used/$disk_total)*100; $formatted_disk_percent = number_format((float)$disk_percentage, 2, '.', ''); ?>
+          <?php
+          $disk_percentage = ($disk_used/$disk_total)*100;
+          $formatted_disk_percent = number_format((float)$disk_percentage, 2, '.', '');
+          $disk_progress_color = "bg-success";
+          if ($formatted_disk_percent > 50) {
+            $disk_progress_color = "bg-warning";
+          }
+          if ($formatted_disk_percent > 90) {
+            $disk_progress_color = "bg-danger";
+          }
+          ?>
           <div class="progress" style="height: 24px;">
-            <div class="progress-bar bg-success" role="progressbar" style="width: <?php echo $formatted_disk_percent ?>%;" aria-valuenow="<?php echo $formatted_disk_percent ?>" aria-valuemin="0" aria-valuemax="100"><?php echo $formatted_disk_percent ?>%</div>
+            <div class="progress-bar <?php echo $disk_progress_color ?>" role="progressbar" style="width: <?php echo $formatted_disk_percent ?>%;" aria-valuenow="<?php echo $formatted_disk_percent ?>" aria-valuemin="0" aria-valuemax="100"><?php echo $formatted_disk_percent ?>%</div>
           </div>
         </div>
       </div>
